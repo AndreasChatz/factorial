3114d13190901da2208ee51357b95c9ffcf55223
AndreasChatz <andreas.chatzivasileiadis@gmail.com>
2017-10-18 18:59:32 +0300

    add test

--- prime_swing.py
+++ prime_swing.py
@@ -4,35 +4,40 @@ import math
 from typing import List
 
 
-def divide_swing_and_conquer(number: int, prime_numbers: int) -> int:
+def divide_swing_and_conquer(number: int) -> int:
 
-    if number == 1:
+    if number == 1 or number == 0:
         return 1
 
-    k = math.floor(math.log2(number))
+    prime_numbers = all_prime_numbers_up_to(number)
 
-    prime_factors_exponent_array = prime_factors_exponent(k, number, prime_numbers)
+    prime_exponents_array = prime_exponents_of_a_swing_number(number,
+                                                              prime_numbers)
 
-    sf = compute_swinging_factorial(prime_numbers, prime_factors_exponent_array)
+    sf = compute_swinging_factorial(prime_numbers, prime_exponents_array)
 
-    return divide_swing_and_conquer(number//2, prime_numbers)**2 * sf
+    return divide_swing_and_conquer(number//2)**2 * sf
 
 
-def prime_factors_exponent(k: int, number: int, prime_numbers: List[int]) -> List[int]:
-    prime_factors_exponent_array = []
+def prime_exponents_of_a_swing_number(swing_number: int,
+                                      prime_numbers: List[int]) -> List[int]:
+    k = math.floor(math.log2(swing_number))
+
+    prime_exponents_array = []
 
     for prime in prime_numbers:
         exponent = 0
         for i in range(1, k+1):
-            exponent += number // prime ** i % 2
-        prime_factors_exponent_array.append(exponent)
+            exponent += swing_number // prime ** i % 2
+        prime_exponents_array.append(exponent)
 
-    return prime_factors_exponent_array
+    return prime_exponents_array
 
 
-def compute_swinging_factorial(prime_numbers: List[int], prime_factors_exponent_array: List[int]) -> int:
+def compute_swinging_factorial(prime_numbers: List[int],
+                               prime_exponents_array: List[int]) -> int:
     swing_factorial = 1
-    for prime, factor in zip(prime_numbers, prime_factors_exponent_array):
+    for prime, factor in zip(prime_numbers, prime_exponents_array):
         if factor == 0:
             continue
 
@@ -79,7 +84,9 @@ def all_prime_numbers_up_to(n: int) -> List[int]:
 if __name__ == "__main__":
     try:
         n = int(input('Factorial of: '))
-        prime_numbers_array = all_prime_numbers_up_to(n)
-        print(divide_swing_and_conquer(n, prime_numbers_array))
+        if n < 0:
+            raise ValueError
+        else:
+            print(divide_swing_and_conquer(n))
     except ValueError:
-        print('Please provide an integer')
+        print('Please provide a positive integer')
--- test_prime_swing.py
+++ test_prime_swing.py
@@ -1,17 +1,78 @@
-# from prime_swing import is_prime
-
 import prime_swing
+from constants import PRIMES_UP_TO_2017
 
 
+def test_is_prime():
+    test_cases = (
+        (1, False),
+        (13, True),
+        (15, False),
+        (8273, True),
+        (8279, False),
+        (15486101, True),
+        (15486127, False),
+        (179425261, True),
+        (179425291, False),
+    )
+
+    for test_case in test_cases:
+        number, result = test_case
+        assert prime_swing.is_prime(number) == result
 
 
-def test_primality():
+def test_all_prime_numbers_up_to_2017():
+    for i, prime in enumerate(prime_swing.all_prime_numbers_up_to(2017)):
+        assert prime == PRIMES_UP_TO_2017[i]
 
-    # print('primality of: -1')
-    # assert is_prime(-1)
 
-    # print('primality of: 0')
-    # assert is_prime(0)
+def test_prime_exponents_of_a_swing_number():
+    # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+    test_cases = (
+        (3,  [1, 1]),
+        (7,  [2, 0, 1, 1]),
+        (15, [3, 2, 1, 0, 1, 1]),
+        (31, [4, 2, 1, 0, 0, 0, 1, 1, 1, 1, 1]),
+        (62, [5, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]),
+    )
+    for test_case in test_cases:
+        swing_number, result = test_case
+        prime_numbers = prime_swing.all_prime_numbers_up_to(swing_number)
+        assert prime_swing.prime_exponents_of_a_swing_number(
+            swing_number, prime_numbers) == result
 
-    print('primality of: 1')
-    assert prime_swing.is_prime(1) is False
+
+def test_compute_swinging_factorial():
+    test_cases = (
+        (3,  2 * 3),
+        (7,  2**2 * 5 * 7),
+        (15, 2**3 * 3**2 * 5 * 11 * 13),
+        (31, 2**4 * 3**2 * 5 * 17 * 19 * 23 * 29 * 31),
+        (62, 2**5 * 7 * 11 * 17 * 19 * 37 * 41 * 43 * 47 * 53 * 59 * 61),
+    )
+
+    for test_case in test_cases:
+        swinging_factorial, result = test_case
+        prime_numbers = prime_swing.all_prime_numbers_up_to(swinging_factorial)
+        prime_exponents_array = prime_swing.prime_exponents_of_a_swing_number(
+            swinging_factorial, prime_numbers)
+
+        assert prime_swing.compute_swinging_factorial(
+            prime_numbers, prime_exponents_array) == result
+
+
+def test_divide_swing_and_conquer():
+    test_cases = (
+        (0, 1),
+        (1, 1),
+        (5, 120),
+        (6, 720),
+        (13, 6227020800),
+        (24, 620448401733239439360000),
+        (42, 1405006117752879898543142606244511569936384000000000),
+        (62, 31469973260387937525653122354950764088012280797258232192163168247821107200000000000000),
+        (100, 93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000),
+    )
+
+    for test_case in test_cases:
+        number, factorial = test_case
+        assert prime_swing.divide_swing_and_conquer(number) == factorial
